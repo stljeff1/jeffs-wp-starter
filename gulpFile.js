69bb69c487bad5192ff456be5aa1dc6702b69b0c
Jeff Wilkerson <stljeff1@users.noreply.github.com>
2019-10-13 09:21:01 -0700

    updating styles task in gulp

--- gulpFile.js
+++ gulpFile.js
@@ -3,17 +3,19 @@
 const gulp = require('gulp');
 const gulpsass = require('gulp-sass');
 const cssnano = require('cssnano');
-const autoprefixer = require('autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');
 const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('styles', () => {
-	return gulp.src(['src/styles/main.scss'])
-		.pipe(gulpsass())
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
+	return gulp.src('src/styles/site.scss')
+		.pipe(sassglob())
+		.pipe(gulpsass({
+			includePaths: [
+				'node_modules/foundation-sites/scss/'
+			]
 		}))
+		.pipe(autoprefixer())
 		.pipe(gulp.dest('./build'));
 });
 
