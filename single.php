c11fc0f14f41d8882b246f0a83c27f357c062dd0
Jeff Wilkerson <stljeff1@users.noreply.github.com>
2019-10-13 09:17:32 -0700

    adding php and twig templates

---
+++ single.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::context();
+$post = Timber::query_post();
+$context['post'] = $post;
+$context['wp_title'] .= ' - ' . $post->title();
+$context['comment_form'] = Timber\Helper::get_comment_form();
+
+if ( post_password_required( $post->ID ) ) {
+	Timber::render( 'pages/single-password.twig', $context );
+} else {
+	Timber::render( array( 'pages/single-' . $post->ID . '.twig', 'pages/single-' . $post->post_type . '.twig', 'pages/single.twig' ), $context );
+}
