6ffc7f14dccb02824d5d5c67187f3084c2646dcb
Jeff Wilkerson <stljeff1@users.noreply.github.com>
2019-10-13 12:37:53 -0700

    adding fontawesome internally

--- gulpFile.js
+++ gulpFile.js
@@ -11,11 +11,25 @@ const sourcemaps = require('gulp-sourcemaps');
 
 const webpack = require('webpack-stream');
 
+const buildDir = './build/';
+const paths = {
+	build: {
+		theme: buildDir + 'theme',
+		fonts: buildDir + 'webfonts' 
+	}
+}
+
+
+gulp.task('copy-fontawesome', () => {
+
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest(paths.build.fonts));
+})
 
 gulp.task('js', () => {
 	return gulp.src('src/js/main.js')
 		.pipe(webpack( require('./webpack.config.js') ))
-		.pipe(gulp.dest('./build'));
+		.pipe(gulp.dest(paths.build.theme));
 });
 
 gulp.task('styles', () => {
@@ -23,11 +37,12 @@ gulp.task('styles', () => {
 		.pipe(sassglob())
 		.pipe(gulpsass({
 			includePaths: [
-				'node_modules/foundation-sites/scss/'
+				'node_modules/foundation-sites/scss/',
+				'node_modules/@fortawesome/'
 			]
 		}))
 		.pipe(autoprefixer())
-		.pipe(gulp.dest('./build'));
+		.pipe(gulp.dest(paths.build.theme));
 });
 
 gulp.task('watch-styles', () => {
@@ -38,6 +53,13 @@ gulp.task('watch-js', () => {
 	return gulp.watch(['src/js/**/*.js'], gulp.series('js'));
 });
 
-gulp.task('watch', gulp.parallel(['watch-styles', 'watch-js']))
+const watchers = gulp.parallel(['watch-styles', 'watch-js']);
 
-gulp.task('default', gulp.parallel(['styles', 'js', 'watch-styles', 'watch-js']))
+const prebuild = gulp.series('copy-fontawesome');
+
+
+gulp.task('watch', watchers);
+
+gulp.task('build', gulp.series( prebuild, gulp.parallel(['styles', 'js']) ));
+
+gulp.task('default', gulp.parallel(['build', 'watch']));
